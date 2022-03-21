aad59357222b16254a884d60571e13709b1e0269
Natalia <natalia.s36.ns@gmail.com>
2022-03-21 18:22:52 -0300

    Adicionando componente Home estilizado com css.

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+
+        <> 
+        <h1 className= "titulo"> Home</h1>
+        <img src="https://images.pexels.com/photos/265667/pexels-photo-265667.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt= "imagem de fundo" className="img" />
+        </>
+    );
+}
+export default Home;
